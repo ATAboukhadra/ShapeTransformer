6456b65ddb5bf370fb67d8cdbdf94dd8e7cb01e9
Ahmed Aboukhadra <ahmed.tawfik96@gmail.com>
2023-01-09 15:27:56 +0100

    slurm

---
+++ slurm.sh
@@ -0,0 +1,5 @@
+srun -K --gpus=1 --time=3-0 \
+--container-mounts=/netscratch/aboukhadra/checkpoints:/checkpoints/,/netscratch/aboukhadra/datasets:/datasets/,/ds-av/public_datasets/honotate_v3/raw:/home2/HO3D_v3,"`pwd`":"`pwd`" \
+--container-image=/netscratch/enroot/nvcr.io_nvidia_pytorch_22.07-py3.sqsh \
+--container-workdir="`pwd`" --cpus-per-gpu=32 --mem=120G --partition=A100-80GB \
+./train.sh
--- train.sh
+++ train.sh
@@ -4,4 +4,5 @@ python main.py \
 --data_root /data/ho/ \
 --batch_size 32 \
 --epochs 1 \
+--hdf5 \
 # --output_folder checkpoints/transformer
