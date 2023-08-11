92c6f82969982dd0c39f6aee19780eba204421c4
Ahmed Aboukhadra <ahmed.tawfik96@gmail.com>
2023-08-11 11:24:42 +0200

    thor

--- models/poseformer.py
+++ models/poseformer.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn.functional as F
 from torch import nn, Tensor
 from models.graformer import GraFormer
-from utils import initialize_masks
+# from utils import initialize_masks
 # class MLP(nn.Module):
 #     """
 #     Args:
