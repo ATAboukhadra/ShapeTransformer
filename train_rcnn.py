2e9e0e2941581810d034a09a567fd6f3add7ca64
Ahmed Aboukhadra <ahmed.tawfik96@gmail.com>
2023-08-16 15:53:09 +0200

    resnet18 rcnn

--- train_rcnn.py
+++ train_rcnn.py
@@ -6,6 +6,8 @@ from tqdm import tqdm
 from utils import AverageMeter
 import os
 from datapipes.utils.collation_functions import collate_sequences_as_dicts
+from torchvision.models import resnet18, ResNet18_Weights
+from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
 
 root = '/ds-av/public_datasets/arctic/td/sequential_resized_allocentric/'
 objects_root = 'dataset/arctic_objects'
@@ -16,16 +18,21 @@ batch_size = 1
 num_workers = 4
 sliding_window_size = 1
 epochs = 5
-num_seqs = 16
 num_kps = 21
 mode = 'allocentric' if 'allocentric' in root else 'all'
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+backbone = 'resnet18'
 
-model = torchvision.models.detection.keypointrcnn_resnet50_fpn(num_keypoints=num_kps, num_classes=24).to(device)
+if backbone == 'resnet50':
+    model = torchvision.models.detection.keypointrcnn_resnet50_fpn(num_keypoints=num_kps, num_classes=24).to(device)
+elif backbone == 'resnet18':
+    backbone = resnet_fpn_backbone(backbone_name='resnet18', weights=ResNet18_Weights.DEFAULT, trainable_layers=5)
+    model = torchvision.models.detection.KeypointRCNN(backbone, num_classes=24, num_keypoints=num_kps).to(device)
+    
 
-train_pipeline, num_samples, decoder, factory = create_pipe(root, objects_root, 'train', mode, 'cpu', sliding_window_size, num_seqs)
+train_pipeline, num_samples, decoder, factory = create_pipe(root, objects_root, 'train', mode, 'cpu', sliding_window_size)
 trainloader = torch.utils.data.DataLoader(train_pipeline, batch_size=batch_size, num_workers=num_workers, collate_fn=collate_sequences_as_dicts)
-val_pipeline, val_count, _, _ = create_pipe(root, objects_root, 'val', mode, 'cpu', sliding_window_size, num_seqs, factory=factory, arctic_decoder=decoder)
+val_pipeline, val_count, _, _ = create_pipe(root, objects_root, 'val', mode, 'cpu', sliding_window_size, factory=factory, arctic_decoder=decoder)
 valloader = torch.utils.data.DataLoader(val_pipeline, batch_size=batch_size, num_workers=num_workers, pin_memory=False, collate_fn=collate_sequences_as_dicts)
 
 optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
